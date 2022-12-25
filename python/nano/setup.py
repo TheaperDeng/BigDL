82160abf63860129a2116ca82a7a2154c5333c46
TheaperDeng <theaperdeng@outlook.com>
2022-12-25 21:12:59 +0800

    update setup

--- python/nano/setup.py
+++ python/nano/setup.py
@@ -112,8 +112,8 @@ def setup_package():
     tensorflow_27_requires += tensorflow_common_requires
 
     # ipex is only avaliable for linux now
-    pytorch_113_requires = ["torch==1.13.0",
-                            "torchvision==0.14.0",
+    pytorch_113_requires = ["torch==1.13.1",
+                            "torchvision==0.14.1",
                             "intel_extension_for_pytorch==1.13.0;platform_system=='Linux'"]
 
     pytorch_112_requires = ["torch==1.12.1",
@@ -139,7 +139,7 @@ def setup_package():
                                "opencv-transforms"]
 
     # default pytorch_dep
-    pytorch_requires = pytorch_112_requires + pytorch_common_requires
+    pytorch_requires = pytorch_113_requires + pytorch_common_requires
     pytorch_113_requires += pytorch_common_requires
     pytorch_112_requires += pytorch_common_requires
     pytorch_111_requires += pytorch_common_requires
@@ -151,8 +151,8 @@ def setup_package():
                           "onnxruntime-extensions==0.4.2; platform_system!='Darwin'",
                           "onnxruntime-extensions==0.3.1; platform_machine=='x86_64' and \
                           platform_system=='Darwin'",
-                          "openvino-dev==2022.2.0",
-                          "neural-compressor==1.13.1",
+                          "openvino-dev==2022.3.0",
+                          "neural-compressor==1.14.2",
                           "onnxsim==0.4.8; platform_system!='Darwin'",
                           "onnxsim==0.4.1; platform_machine=='x86_64' and \
                           platform_system=='Darwin'"]
